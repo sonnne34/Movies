54f69738aba15c1803cfd286704205b12ad510ce
sonne <sonnne34@gmil.com>
2022-10-27 14:36:31 +0500

    add Adapter RecyclerView

--- app/src/main/java/com/sonne/movies/MainActivity.java
+++ app/src/main/java/com/sonne/movies/MainActivity.java
@@ -1,29 +1,37 @@
 package com.sonne.movies;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
     private MainViewModel mainViewModel;
+
+    private RecyclerView recyclerView;
+    private MoviesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.rvMovies);
+        adapter = new MoviesAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         mainViewModel.movieList().observe(this, new Observer<List<Movie>>() {
             @Override
             public void onChanged(List<Movie> movies) {
-                Log.d(TAG, movies.toString());
+                adapter.setMovieList(movies);
             }
         });
         mainViewModel.loadMovies();
---
+++ app/src/main/java/com/sonne/movies/MoviesAdapter.java
@@ -0,0 +1,64 @@
+package com.sonne.movies;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
+
+    private List<Movie> movieList = new ArrayList<>();
+
+    public void setMovieList(List<Movie> movieList) {
+        this.movieList = movieList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.item_movie,
+                parent,
+                false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie movie = movieList.get(position);
+        Glide.with(holder.itemView)
+                .load(movie.getPoster().getUrl())
+                .into(holder.imageViewPoster);
+
+        holder.textViewRating.setText(movie.getRating().getKp());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    static class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView imageViewPoster;
+        private final TextView textViewRating;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageViewPoster = itemView.findViewById(R.id.imageViewPoster);
+            textViewRating = itemView.findViewById(R.id.textViewRating);
+        }
+    }
+}
