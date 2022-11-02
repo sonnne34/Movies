d5dbe1c9ef213edc968092b744e6d96f8228049d
sonne <sonnne34@gmil.com>
2022-11-02 16:12:02 +0500

    add reviews color

--- app/src/main/java/com/sonne/movies/ReviewsAdapter.java
+++ app/src/main/java/com/sonne/movies/ReviewsAdapter.java
@@ -1,17 +1,23 @@
 package com.sonne.movies;
 
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ReviewViewHolder> {
+
+    private static final String TYPE_POSITIVE = "Позитивный";
+    private static final String TYPE_NEGATIVE = "Негативный";
 
     private List<Review> reviewList = new ArrayList<>();
 
@@ -37,6 +43,17 @@ public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ReviewVi
         Review review = reviewList.get(position);
         holder.tvAuthor.setText(review.getAuthor());
         holder.tvReview.setText(review.getReview());
+        String reviewType = review.getType();
+        int backgroundId;
+        if (reviewType.equals(TYPE_POSITIVE)) {
+            backgroundId = R.color.green;
+        } else if (reviewType.equals(TYPE_NEGATIVE)) {
+            backgroundId = R.color.red;
+        } else {
+            backgroundId = R.color.orange;
+        }
+        Drawable background = ContextCompat.getDrawable(holder.itemView.getContext(), backgroundId);
+        holder.llReview.setBackground(background);
     }
 
     @Override
@@ -47,11 +64,13 @@ public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ReviewVi
     static class ReviewViewHolder extends RecyclerView.ViewHolder {
         private final TextView tvAuthor;
         private final TextView tvReview;
+        private final LinearLayout llReview;
 
         public ReviewViewHolder(@NonNull View itemView) {
             super(itemView);
             tvAuthor = itemView.findViewById(R.id.textViewAuthor);
             tvReview = itemView.findViewById(R.id.textViewReview);
+            llReview = itemView.findViewById(R.id.llReview);
         }
     }
 }
